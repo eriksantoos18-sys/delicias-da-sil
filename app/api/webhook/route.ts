e040f3699f418f2811be6bf0645b59b2f792ce3b
eriksantoos18-sys <eriksantoos18@gmail.com>
2026-05-23 17:17:31 -0300

    att whatsapp

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -11,7 +11,8 @@ async function enviarWhatsapp(
 ) {
   const numero = telefone.replace(/\D/g, "");
 
-  await fetch(
+  const response = await fetch(
+
     `https://graph.facebook.com/v25.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`,
     {
       method: "POST",
@@ -29,7 +30,15 @@ async function enviarWhatsapp(
       }),
     }
   );
+  
+  const data = await response.json();
+
+console.log("WHATSAPP STATUS:", response.status);
+console.log("WHATSAPP RESPOSTA:", data);
 }
+
+
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
